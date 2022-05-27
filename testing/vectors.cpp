f71bda5a8165ca12bf7c96c0d3aa52cf300151c6
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2022-05-27 09:08:56 +0200

    Create vectors.cpp

---
+++ testing/vectors.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Student {
+        private:
+                string name;
+                string id;
+               /* vector<int> grades; */
+
+        public:
+
+                vector<int> grades;
+                        
+                Student(string name_c, string id_c) {
+                        name = name_c;
+                        id = id_c;
+                }
+
+                string getName(){return name;}
+                string getId(){return id;}
+                
+
+                void addGrade(int grade) {
+                        grades.push_back(grade);
+                }
+
+};
+
+int main() {
+        Student newStudent("jirka", "R34TZFBVCXDHT");
+        cout << newStudent.getName() << " | " << newStudent.getId() << "\n";
+        newStudent.addGrade(3);
+        newStudent.addGrade(1);
+        cout << newStudent.grades.size();
+} 
