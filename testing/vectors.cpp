85cb4fe49b6f30fa21e5639e0865ca696a1f019f
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2022-06-27 17:28:29 +0200

    Update vectors.cpp

--- testing/vectors.cpp
+++ testing/vectors.cpp
@@ -44,5 +44,15 @@ int main() {
         newStudent.displayGrades();
 } 
 
-// std::vector<std::array<int, 2>> vp = { {{1,1}}, {{0,0}} }; ARRAYS INSIDE ARRAY, MAXIMAL LENGHT OF SUB_ARRAY IS 2(#include <array>)
+// std::vector<std::array<int, 2>> screen = { {{1,1}}, {{0,0}} }; ARRAYS INSIDE ARRAY, MAXIMAL LENGHT OF SUB_ARRAY IS 2(#include <array>)
 // std::vector<std::vector<int>> screen = { {{9, 13}}, {{32, 1, 23}} }; VECTOR ARRAYS 
+
+/* FOR LOOP
+
+for (auto const &row : screen) {
+        for (auto cell : row) {
+               // do something
+        }
+}
+
+*/
